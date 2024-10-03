8ff8e7658340ebb543598a13b73aa6801efbb38e
Emmayeng The GREATEST <sheilalebene6@gmail.com>
2024-10-03 12:39:19 +0100

    Add files via upload

---
+++ FONEMATiC LEAD GENERATOR/Setup/installer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo -e "              0000_____________0000________0000000000000000__000000000000000000+\n            00000000_________00000000______000000000000000__0000000000000000000+\n           000____000_______000____000_____000_______0000__00______0+\n          000______000_____000______000_____________0000___00______0+\n         0000______0000___0000______0000___________0000_____0_____0+\n         0000______0000___0000______0000__________0000___________0+\n         0000______0000___0000______0000_________000___0000000000+\n         0000______0000___0000______0000________0000+\n          000______000_____000______000________0000+\n           000____000_______000____000_______00000+\n            00000000_________00000000_______0000000+\n              0000_____________0000________000000007;"
+
+echo "Welcome to FONEMATICS lead Generator Installer"
+
+sleep 4
+
+echo "Enter your license key: "
+read key
+
+#if ["$user_input" = "QA3AW8U62Z4ZWTSPV44VXI65P59OLE547WHIQVZYWLARL9JEYQEGDUBLH8Z3ZCJAL3FLXMS98V95TSDYI7FOEXUPRR" ]; then
+sleep 5 && echo "Verifying keyphrase, please wait..."
+sleep 4
+echo "Error this keyphrase has been registered to a maximum of 5 devices. Kindly enter another key"
+sleep 4
+
+echo "Error this keyphrase has been registered to a maximum of 5 devices. Kindly enter another key"
+sleep 4
+
+echo "Error this keyphrase has been registered to a maximum of 5 devices. Kindly enter another key"
+sleep 4
+
+
---
+++ FONEMATiC LEAD GENERATOR/beforeinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+echo "Enter your key:"
+
+
+
+
+read key
+
+# Pause for another 5 seconds
+sleep 5
+
+
+echo "Troubleshooting starts now please give up to an hour and run fonemaics generator again"
+
+sleep 5
